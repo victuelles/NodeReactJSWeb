35c798b8c7597a7dac2dfe840100537f1b514de3
Rom Vic <technoyguy@gmail.com>
2018-10-23 15:18:02 -0700

    prod keys added

--- services/passport.js
+++ services/passport.js
@@ -3,7 +3,20 @@ const GoogleStrategy=require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
 const keys= require('../config/keys');
 
-const User = mongoose.model('users')
+const User = mongoose.model('users');
+
+passport.serializeUser((user,done)=>{
+    //user.id -> mongo record _id/user model instance id
+   done(null,user.id);
+
+});
+//id = mongo record id
+passport.deserializeUser((id, done)=>{
+    User.findById(id)
+    .then(user=>{
+        done(null,user);
+    });
+});
 
 passport.use(
     new GoogleStrategy({
@@ -16,13 +29,16 @@ passport.use(
          .then((existingUser)=>{
              if (existingUser) {
                 //we already have a record with the given profile ID
+                //two parameters. error =null, record=existingUser
+                done(null,existingUser);
              }else{
-                //we dont have a user record, create one
-                new User({googleId:profile.id}).save();
+                //we dont have a user record, create one, async call
+                new User({googleId:profile.id})
+                .save()
+                .then((newUser)=>{done(null,newUser)});
              }
-         })
-
-      
+         });
+    
         
     })
 );
